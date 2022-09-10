53e0366ecc1f362b688baa5f2f1a95ad2467b5fb
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-09-09 19:57:20 -0700

    finished code-challenges

--- code-challenges.js
+++ code-challenges.js
@@ -20,7 +20,8 @@ const fruit2 = "banana"
 
 // --------------------1) Create the code that determines if a given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
-// Describe your process: I created a conditional else if statement using the constant variable "temp" to determine if the number inputted is 212, above 212, or below 212. Using an if statement if "temp" is exactly equal to (using ===) 212 then the console log, using string interpolation to retrieve the number inputted in the constant variable, will say "212 is at boiling point". I then used else if statements to see if temp is below 212 (using <) or above 212 (using >).
+// Describe your process: I created a conditional else if statement using the constant variable "temp" to determine if the number inputted is 212, above 212, or below 212. Using an if statement if "temp" is exactly equal to (using ===) 212 then the console log, using string interpolation to retrieve the number inputted in the constant variable, will say "212 is at boiling point". I then used else if statements to see if temp is below 212 (using <) or above 212 (using >). Finally I added a final else to output an Error! if anything else was in the "temp" variable.
+
 
 const temp = 42
 if (temp === 212) {
@@ -29,15 +30,34 @@ if (temp === 212) {
     console.log (`${temp} is below boiling point`)
 } else if (temp > 212) {
     console.log (`${temp} is above boiling point`)
+} else {
+    console.log ("Error!")
 }
 
 // Expected output: "42 is below boiling point"
 
 // const temp = 350
+// if (temp === 212) {
+//     console.log (`${temp} is at boiling point`)
+// } else if (temp < 212) {
+//     console.log (`${temp} is below boiling point`)
+// } else if (temp > 212) {
+//     console.log (`${temp} is above boiling point`)
+// }
+//
 // Expected output: "350 is above boiling point"
 
 // const temp = 212
+// if (temp === 212) {
+//     console.log (`${temp} is at boiling point`)
+// } else if (temp < 212) {
+//     console.log (`${temp} is below boiling point`)
+// } else if (temp > 212) {
+//     console.log (`${temp} is above boiling point`)
+// }
+//
 // Expected output: "212 is at boiling point"
+
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
@@ -51,32 +71,40 @@ console.log(padres1984WorldSeriesRuns.length + padres1998WorldSeriesRuns.length)
 
 // Expected output: 9
 
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Describe your process:
+// Describe your process: I put first put currentCohort into a console.log with the split accessor to split the string into an array, as the reverse function would not work with a string. Now that "Foxtrot 2022" is split and seperated into an array, I used .reverse to reverse the array. Finally I used .join to reconnect the now reversed array back into a string to output 2202 tortxoF.
 
 const currentCohort = "Foxtrot 2022"
+console.log(currentCohort.split("").reverse("").join(""))
 
 
 // Expected output: "2202 tortxoF"
 
+
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
-// Describe your process:
+// Describe your process: To return the last index of a given value from this array we must use lastIndexOf in the console log, as there are repeat numbers and using indexOf would bring us the first value which is not what we want. 
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
+console.log(myNumbers.lastIndexOf(givenValue1))
 // Expected output: 7
 
 const givenValue2 = 10
+console.log(myNumbers.lastIndexOf(givenValue2))
 // Expected output: 8
+
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
-// Describe your process:
+// Describe your process: I first put the variable sanDiegoSummerTemperatures inside of a console.log and then used the .sort accessor to sort the numbers in order. This gave me the array that was from smallest to largest, so similarily with question #3 I added another accessor to the console log to reverse the order to largest to smallest which was .reverse.  
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+console.log(sanDiegoSummerTemperatures.sort().reverse())
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+console.log(sanDiegoWinterTemperatures.sort().reverse())
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- coding-concepts.js
+++ coding-concepts.js
@@ -27,33 +27,36 @@ const cohort = "LEARN 2022"
 const greeting = "Hello World!"
 //console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This should give the 5th letter in the string "Hello World!" which would be o  
+// b) Verify and explain: It did in fact output the letter o as it was the index of 4.
 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It should say just say Ruby, I'm not sure if languages["Ruby"] would do anything differently.
+// b) Verify and explain: It does say Ruby. Ruby is the index of 1, and that is what is in the console log.
 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It should say SATURDAY, SUNDAY, now uppercased because of the .toUpperCase in the console.log
+// b) Verify and explain: It instead came with an error due to ["saturday", "sunday"] being the improper syntax for the .toUpperCase() function. I believe the following is the correct way:
+//const weekendDays = ('saturday, sunday')
+//console.log(weekendDays.toUpperCase())
+//output: SATURDAY, SUNDAY
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
 // a) Your answer:
 // b) Verify and explain:
